8542afdb2a1a5366e590833e81225daa93ddcdcf
Marlon Guimaraes <marlonciriatico@gmail.com>
2023-04-11 01:26:37 -0300

    show pages on menu popup

--- react/popup.tsx
+++ react/popup.tsx
@@ -1,18 +1,40 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
+const divStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+  width: '300px',
+  gap: '30px',
+};
+
 const Popup = () => {
-    return (
-        <div>
-            <button type='button' onClick={() => alert('popup.html')}>
-                Click me! (Popup.html)
-            </button>
-        </div>
-    );
+  return (
+    <div style={divStyle}>
+      <button type='button' onClick={() => alert('popup.html')}>
+        Click me! (react/popup.tx)
+      </button>
+
+      <button
+        type='button'
+        onClick={() => chrome.runtime.openOptionsPage(() => {})}>
+        Options page (react/options.tsx)
+      </button>
+
+      <button
+        type='button'
+        onClick={() => {
+          chrome.tabs.create({ active: true, url: 'html/localpage.html' });
+        }}>
+        Options custom local page (react/localpage.tsx)
+      </button>
+    </div>
+  );
 };
 
 function App() {
-    return <Popup />;
+  return <Popup />;
 }
 
 const mountNode = document.getElementById('popup-app');
